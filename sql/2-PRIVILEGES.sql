ffcba1325e0f0edc93058b7d5301f7d58d308adc
Matthieu Lemaile <mlemaile@excilys.com>
2017-05-19 15:01:30 +0200

    Ajout des dépendances qui seront nécéssaires

--- sql/2-PRIVILEGES.sql
+++ sql/2-PRIVILEGES.sql
@@ -3,6 +3,6 @@
   #-----------------------------------
   CREATE USER 'adminippie'@'localhost' IDENTIFIED BY 'qwerty1234';
 
-  GRANT ALL PRIVILEGES ON `ippie-db`.* TO 'admincdb'@'localhost' WITH GRANT OPTION;
+  GRANT ALL PRIVILEGES ON `ippie-db`.* TO 'adminippie'@'localhost' WITH GRANT OPTION;
 
   FLUSH PRIVILEGES;
