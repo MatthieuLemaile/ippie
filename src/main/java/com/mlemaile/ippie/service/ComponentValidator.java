0d7c40a3562688e5592c0486cf6ff1221e5e4d20
Matthieu Lemaile <matlem35@yahoo.fr>
2017-09-01 21:15:23 +0200

    [FEATURE] Adding rules for component validation

--- src/main/java/com/mlemaile/ippie/service/ComponentValidator.java
+++ src/main/java/com/mlemaile/ippie/service/ComponentValidator.java
@@ -1,5 +1,8 @@
 package com.mlemaile.ippie.service;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -17,9 +20,30 @@ public class ComponentValidator implements Validator {
 
     @Override
     public void validate ( Object target, Errors errors ) {
+        DateTimeFormatter df = DateTimeFormatter.ISO_LOCAL_DATE;
         ValidationUtils.rejectIfEmpty(errors, "name",
                 "The name can't be null, empty or white space.");
-        // TODO for other rules, see with PO.
+        ComponentDto compo = (ComponentDto) target;
+        if (compo.getModelId() <= 0) {
+            errors.rejectValue("Modèle", "You must specify a Model");
+        }
+        if (compo.getIntroduced() != null && !compo.getIntroduced().isEmpty()) {
+            try {
+                df.parse(compo.getIntroduced());
+            } catch (DateTimeParseException e) {
+                errors.rejectValue("Introduced date",
+                        "The date must be in the following format : yyyy-mm-dd");
+            }
+        }
+        if (compo.getDiscontinued() != null && !compo.getDiscontinued().isEmpty()) {
+            try {
+                df.parse(compo.getDiscontinued());
+            } catch (DateTimeParseException e) {
+                errors.rejectValue("Discontinued date",
+                        "The date must be in the following format : yyyy-mm-dd");
+            }
+        }
+        // TODO Ensure the state is in the posible list
     }
 
 }
