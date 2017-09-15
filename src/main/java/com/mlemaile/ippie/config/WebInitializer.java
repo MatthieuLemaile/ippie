1333274c09382addf8d06f614e304fc07b49b8cb
Matthieu Lemaile <matlem35@yahoo.fr>
2017-09-15 15:07:35 +0200

    [FIX] No more web.xml

--- src/main/java/com/mlemaile/ippie/config/WebInitializer.java
+++ src/main/java/com/mlemaile/ippie/config/WebInitializer.java
@@ -1,15 +1,25 @@
 package com.mlemaile.ippie.config;
 
 
+import javax.servlet.Filter;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Configuration
 @ComponentScan(basePackages = { "com.mlemaile.ippie" })
 @EnableTransactionManagement
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Filter[] getServletFilters() {
+      return new Filter[] {
+                new CharacterEncodingFilter("UTF-8", true)
+      };
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses () {
